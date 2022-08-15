9de554d01218a4339432290cd0cf281f00817d3a
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-15 20:25:07 +0530

    Welcome to Address Book Program

---
+++ src/com/intellijide/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.intellijide.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to Address Book Program");
+    }
+}
