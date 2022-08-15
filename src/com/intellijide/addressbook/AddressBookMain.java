6d123047d245548a7a8dfef20e2bedb84b5118a9
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-16 00:41:30 +0530

    Added: ability to add new contact

--- src/com/intellijide/addressbook/AddressBookMain.java
+++ src/com/intellijide/addressbook/AddressBookMain.java
@@ -3,9 +3,16 @@ package com.intellijide.addressbook;
 import java.util.Scanner;
 
 public class AddressBookMain {
+
+    Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("welcome to Address Book Program");
         Scanner scan = new Scanner(System.in);
+        AddressBookMain abm = new AddressBookMain();
+        abm.personDetails();
+        System.out.println();
+    }
+    void personDetails(){
         System.out.println("Enter first and last name -> ");
         String firstName = scan.next();
         String lastName = scan.next();
@@ -27,5 +34,16 @@ public class AddressBookMain {
         System.out.println("city -> " +city);
         System.out.println("zip code -> " +zip);
         System.out.println("Phone Number -> " +phoneNumber);
+
+        System.out.println("Enter 1 to Add Details of new person");
+        System.out.println("Enter 2 to exit");
+        int choice = scan.nextInt();
+        switch (choice){
+            case 1:
+                personDetails();
+                break;
+            case 2:
+                System.out.println("Exited from Address book");
+        }
     }
 }
