957d5621eb3582baadd144a5d473217ae7ea343e
AniketDShelar <theaniketshelar@gmail.com>
2022-09-11 20:25:03 +0530

    search persons by city

--- src/com/intellijide/addressbook/AddressBookMain.java
+++ src/com/intellijide/addressbook/AddressBookMain.java
@@ -1,5 +1,6 @@
 package com.intellijide.addressbook;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,14 +9,16 @@ public class AddressBookMain {
 
     static final String FAMILY_ADDRESS_BOOK = "Family Address Book";
     static final String PROFESSIONAL_ADDRESS_BOOK = "Professional Address Book";
+
     public static void main(String[] args) {
         int userInput;
         Scanner scan = new Scanner(System.in);
         Map<String,AddressBook> addressBookMap = new HashMap<>();
         AddressBook familyAddressBook = new AddressBook();
         AddressBook professionalAddressBook = new AddressBook();
+        ArrayList<AddressBook> addressBookList = new ArrayList<>();
         do{
-            System.out.println("Enter 1 to Add Family contact\nEnter 2 to Add Professinal contact\nEnter 3 to Edit contact\nEnter 4 to Delete contact\nEnter 0 to Exit");
+            System.out.println("Enter 1 to Add Family contact\nEnter 2 to Add Professinal contact\nEnter 3 to Edit contact\nEnter 4 to Delete contact\nEnter 5 to get list by city\nEnter 0 to Exit");
             userInput = scan.nextInt();
             switch (userInput){
                 case 1:
@@ -36,6 +39,11 @@ public class AddressBookMain {
                     familyAddressBook.deleteContacts();
                     System.out.println(familyAddressBook.contactList);
                     break;
+                case 5:
+                    System.out.println("Enter Name of City: ");
+                    String city = scan.next();
+                    familyAddressBook.getPersonNameByCity(city);
+                    break;
                 default:
                     System.out.println("Exited from Address Book");
             }
@@ -45,6 +53,11 @@ public class AddressBookMain {
         addressBookMap.put(FAMILY_ADDRESS_BOOK, familyAddressBook);
         addressBookMap.put(PROFESSIONAL_ADDRESS_BOOK, professionalAddressBook);
 
+        addressBookList.add(familyAddressBook);
+        addressBookList.add(professionalAddressBook);
+
+        System.out.println(addressBookList);
         System.out.println(addressBookMap.toString());
+
     }
 }
